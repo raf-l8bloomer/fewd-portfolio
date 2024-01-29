cc5becfeb553251f71bcaaf1af1ace20c11cc6f0
raf <lumang.rafael@gmail.com>
2024-01-28 20:01:57 -0800

    added dark theme toggle

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-const time = document.getElementById('time');
+const time = document.querySelector('#time');
 
 function updateDateTime() {
     const date = new Date();
@@ -11,5 +11,41 @@ function updateDateTime() {
 setInterval(updateDateTime, 1000);
 
 
+const invert = document.querySelector('#invert');
+const footer = document.querySelector('.footer');
+const h1 = document.getElementsByTagName('h1');
+const h2 = document.getElementsByTagName('h2');
+const h3 = document.getElementsByTagName('h3')
+const h4 = document.getElementsByTagName('h4')
+const icons = document.getElementsByClassName('social');
+const iconsArray = [...icons];
+let headings = [];
+headings = [...headings, ...h1, ...h2, ...h3, ...h4];
+console.log(headings)
 
 
+
+invert.addEventListener('click', () => {
+if (invert.checked) {
+    console.log('checkbox works')
+
+    document.body.style.backgroundColor = "#E43E22"
+    footer.style.backgroundColor = "#E43E22"
+    headings.forEach(heading => {
+        heading.style.color = "#E5EBE4"
+    })
+    iconsArray.forEach(icon => {
+        icon.setAttribute("fill", "#E5EBE4")
+    })
+
+} else {
+    document.body.style.backgroundColor = "#E5EBE4";
+    footer.style.backgroundColor = "#E5EBE4"
+    headings.forEach(heading => {
+        heading.style.color = "#E43E22"
+    })
+    iconsArray.forEach(icon => {
+        icon.setAttribute("fill", "#E43E22")
+    })
+}
+})
